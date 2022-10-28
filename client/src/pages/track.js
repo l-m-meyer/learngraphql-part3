46fdd7d90a6627744f10d54f70cbf1c2486a7bc7
Lisa Meyer <lisamermey@gmail.com>
2022-10-28 15:36:22 -0400

    Create track.js file

---
+++ client/src/pages/track.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Layout, QueryResult } from '../components';
+
+const Track = () => {
+
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
+
+export default Track;
